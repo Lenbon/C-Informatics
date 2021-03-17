265de15bd057d16d75443ac0607914d8f4ffdbe4
LenBon <alena.bohun@gmail.com>
2021-03-17 16:19:09 +0100

    Add files via upload

---
+++ rallyNC.cpp
@@ -0,0 +1,21 @@
+#include <iostream>
+
+using namespace std;
+ int main()
+ {
+
+ 
+ int n, m, t;
+ 
+ cin >> n >> m;
+ 
+ 
+  
+  t = ( m + n -1) / n; // vremia
+  
+  cout << t << endl;
+  
+  
+ return(0);
+
+ }
